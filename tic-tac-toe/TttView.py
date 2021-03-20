2a12c66e6e953402c5d2319c6bdf7f93b4d4a913
Tim Stoop <tim@kumina.nl>
2021-03-20 15:33:42 +0100

    Make a little nicer

--- tic-tac-toe/TttView.py
+++ tic-tac-toe/TttView.py
@@ -3,8 +3,8 @@ import sys
 
 class TttView:
     def draw(self, out=sys.stdout):
-        out.write(" | | \n")
-        out.write("-|-|-\n")
-        out.write(" | | \n")
-        out.write("-|-|-\n")
-        out.write(" | | \n")
+        out.write("   |   |   \n")
+        out.write("---|---|---\n")
+        out.write("   |   |   \n")
+        out.write("---|---|---\n")
+        out.write("   |   |   \n")

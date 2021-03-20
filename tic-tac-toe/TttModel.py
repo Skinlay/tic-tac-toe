c10e2b9fa5adb8ed86310546721a3c11a150c117
Caitlin Bond <caitlin@kumina.nl>
2021-03-20 15:30:09 +0100

    fail drawings

---
+++ tic-tac-toe/TttModel.py
@@ -0,0 +1,4 @@
+
+
+class TttModel:
+    pass
---
+++ tic-tac-toe/TttView.py
@@ -0,0 +1,10 @@
+import sys
+
+
+class TttView:
+    def draw(self, out=sys.stdout):
+        out.write(" | | \n")
+        out.write("-|-|-\n")
+        out.write(" | | \n")
+        out.write("-|-|-\n")
+        out.write(" | | \n")

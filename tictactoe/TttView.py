4bbef8eef43dcc6f7b9cb94a15f2264794dd1e2b
Tim Stoop <tim@kumina.nl>
2021-03-20 17:06:15 +0100

    Refactor

--- tictactoe/TttView.py
+++ tictactoe/TttView.py
@@ -3,8 +3,9 @@ import sys
 
 class TttView:
     def draw(self, out=sys.stdout):
+        horizontal_separator = "---|---|---\n"
         out.write("   |   |   \n")
-        out.write("---|---|---\n")
+        out.write(horizontal_separator)
         out.write("   |   |   \n")
-        out.write("---|---|---\n")
+        out.write(horizontal_separator)
         out.write("   |   |   \n")

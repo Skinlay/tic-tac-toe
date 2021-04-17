b4dea3cc2e95d4146b172dba5470647e483d2f3b
Caitlin Bond <caitlin@kumina.nl>
2021-04-17 15:06:11 +0200

    added turns and can be tested

--- tests/test_view.py
+++ tests/test_view.py
@@ -2,9 +2,22 @@ import unittest
 from io import StringIO
 from tictactoe import TttView
 
+
 class TestTttView(unittest.TestCase):
     def test_tekeningen(self):
         view = TttView()
         local_out = StringIO()
         view.draw(out=local_out)
-        self.assertEqual(local_out.getvalue(), "   |   |   \n---|---|---\n   |   |   \n---|---|---\n   |   |   \n")
+        self.assertEqual(local_out.getvalue(),
+                         " 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n")
+
+    def test_turn_incorect(self):
+        view = TttView()
+        view.turn = "N"
+        local_out = StringIO()
+        with self.assertRaises(SystemExit) as err:
+            view.which_turn(out=local_out)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
--- tictactoe/TttView.py
+++ tictactoe/TttView.py
@@ -5,6 +5,7 @@ class TttView:
 
     def __init__(self):
         self.list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.turn = "x"
 
     def draw(self, out=sys.stdout):
         horizontal_separator = "\n---|---|---\n"
@@ -18,8 +19,16 @@ class TttView:
         out.write(horizontal_separator)
         out.write(txt3)
         out.write("\n")
+        out.write("")
 
     def drawline(self, ln):
         n = ln*3
         txt = " {} | {} | {} ".format(self.list[n], self.list[n+1], self.list[n+2])
         return txt
+
+    def which_turn(self, out=sys.stdout):
+        if self.turn in ["x", "o"]:
+            return self.turn
+        else:
+            out.write("turn no")
+            sys.exit(1)

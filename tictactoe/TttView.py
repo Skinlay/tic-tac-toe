f3765e5f669576ba9eebaf698efae7b6c112b67a
Tim Stoop <tim@kumina.nl>
2021-05-02 14:56:52 +0200

    Add type hints

--- tictactoe/TttView.py
+++ tictactoe/TttView.py
@@ -1,16 +1,17 @@
 import sys
 import os
+from io import StringIO
 
 
 class TttView:
-
-    def __init__(self):
+    def __init__(self: TttView) -> None:
         self.list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         self.turn = "x"
         self.rounds = 0
         self.antwoord = -1
 
-    def play(self):
+    def play(self: TttView) -> None:
+        """Start playing the game."""
         while self.rounds < 4:
             self.rounds = self.rounds + 1
             self.draw()
@@ -18,7 +19,10 @@ class TttView:
             self.placing_choice()
             self.change_drawn_turn()
 
-    def draw(self, out=sys.stdout):
+    def draw(self: TttView, out: StringIO = sys.stdout) -> None:
+        """
+        Draw the board on the out variable.
+        """
         os.system('clear')
         horizontal_separator = "\n---|---|---\n"
         txt1 = self.drawline(0)
@@ -33,29 +37,29 @@ class TttView:
         out.write("\n")
         out.write("")
 
-    def drawline(self, ln):
+    def drawline(self: TttView, ln: int) -> str:
         n = ln * 3
         txt = " {} | {} | {} ".format(self.list[n], self.list[n + 1], self.list[n + 2])
         return txt
 
-    def which_turn(self, out=sys.stdout):
+    def which_turn(self: TttView, out: StringIO = sys.stdout) -> str:
         if self.turn in ["x", "o"]:
             return self.turn
         else:
             out.write("turn no")
             sys.exit(1)
 
-    def user_input(self):
-        self.antwoord = int(input()) - 1
+    def user_input(self: TttView) -> None:
+        self.antwood = int(input()) - 1
 
-    def placing_choice(self, out=sys.stdout):
+    def placing_choice(self, out: StringIO = sys.stdout) -> None:
         print("round", self.rounds)
-        self.list[self.antwoord] = self.turn
+        self.list[self.antwood] = self.turn
         out.write(str(self.list))
 
     #     if user inputs 0 9 wil be changed
 
-    def change_drawn_turn(self):
+    def change_draw_turn(self: TttView) -> None:
         if self.turn == "x":
             self.turn = "o"
         else:
